49d8dec58b499f6c2f7a353fe6290e7424d94c0d
Abdulrahman <abdulrahman.a.m.a.k.160@gmail.com>
2025-08-04 21:38:44 +0300

    feat: add LinguAI project and updat cv

--- script.js
+++ script.js
@@ -1,32 +1,35 @@
 import { TypeWriter } from "./type-writer.js";
 
 function updateWindowHeight() {
-  document.documentElement.style.setProperty("--vh", `${window.innerHeight}px`);
-  updateScrollElements();
+    document.documentElement.style.setProperty(
+        "--vh",
+        `${window.innerHeight}px`
+    );
+    updateScrollElements();
 }
 function updateScrollElements() {
-  const currScroll = window.scrollY,
-    scrollToTopPos =
-      document.documentElement.scrollHeight - window.innerHeight - 80;
+    const currScroll = window.scrollY,
+        scrollToTopPos =
+            document.documentElement.scrollHeight - window.innerHeight - 80;
 
-  currScroll > 1
-    ? knowMoreButton.classList.add("toggled")
-    : knowMoreButton.classList.remove("toggled");
+    currScroll > 1
+        ? knowMoreButton.classList.add("toggled")
+        : knowMoreButton.classList.remove("toggled");
 
-  currScroll >= scrollToTopPos
-    ? scrollToTopButton.classList.remove("hidden")
-    : scrollToTopButton.classList.add("hidden");
+    currScroll >= scrollToTopPos
+        ? scrollToTopButton.classList.remove("hidden")
+        : scrollToTopButton.classList.add("hidden");
 }
 
 const isMobile =
-  ("ontouchstart" in window || navigator.maxTouchPoints > 0) &&
-  window.innerWidth < 768;
+    ("ontouchstart" in window || navigator.maxTouchPoints > 0) &&
+    window.innerWidth < 768;
 
 const typeWriter = new TypeWriter();
 const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) entry.target.classList.add("revealed");
-  });
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) entry.target.classList.add("revealed");
+    });
 });
 
 const scrollHiddenElements = document.querySelectorAll(".scroll-hidden");
@@ -36,21 +39,21 @@ const heroTitle = document.querySelector(".hero-title");
 
 scrollHiddenElements.forEach((el) => observer.observe(el));
 
-typeWriter.write(heroTitle.children[0], 50, 15, 30);
+typeWriter.write(heroTitle.children[0], 51, 15, 30);
 typeWriter.write(heroTitle.children[1], 50);
 
 updateWindowHeight();
 
 window.addEventListener("scroll", () => {
-  if (isMobile && window.scrollY < 10) updateWindowHeight();
-  else updateScrollElements();
+    if (isMobile && window.scrollY < 10) updateWindowHeight();
+    else updateScrollElements();
 });
 window.addEventListener("resize", () => {
-  if (!isMobile) updateWindowHeight();
-  else if (window.scrollY < 10) updateWindowHeight();
+    if (!isMobile) updateWindowHeight();
+    else if (window.scrollY < 10) updateWindowHeight();
 });
 
 scrollToTopButton.addEventListener("click", () => {
-  updateWindowHeight();
-  window.scrollTo(0, 0);
+    updateWindowHeight();
+    window.scrollTo(0, 0);
 });
--- type-writer.js
+++ type-writer.js
@@ -1,87 +1,104 @@
 export class TypeWriter {
-  counter = 0;
-  text = "";
-  element = null;
-  queue = [];
+    counter = 0;
+    text = "";
+    element = null;
+    queue = [];
 
-  write(
-    element,
-    time = 100,
-    primaryColorStart = null,
-    primaryColorEnd = null,
-    useElementText = true,
-    text = ""
-  ) {
-    if (this.element != null && this.element != element) {
-      const queueItem = {
-        element: element,
-        time: time,
-        primaryColorStart: primaryColorStart,
-        primaryColorEnd: primaryColorEnd,
-        useElementText: false,
-        text: element.innerText,
-      };
-      this.queue.push(queueItem);
-      element.innerHTML = "";
-      return;
+    write(
+        element,
+        time = 100,
+        primaryColorStart = null,
+        primaryColorEnd = null,
+        useElementText = true,
+        text = ""
+    ) {
+        if (this.element != null && this.element != element) {
+            const queueItem = {
+                element: element,
+                time: time,
+                primaryColorStart: primaryColorStart,
+                primaryColorEnd: primaryColorEnd,
+                useElementText: false,
+                text: element.innerText,
+            };
+            this.queue.push(queueItem);
+            element.innerHTML = "";
+            return;
+        }
+
+        if (this.counter === 0) {
+            this.element = element;
+            this.element.classList.add("writing");
+            this.text = useElementText ? element.innerText : text;
+            this.element.innerHTML = "";
+        }
+
+        if (this.counter < this.text.length) {
+            let char = this.text.charAt(this.counter);
+            let delay = /[.,،؟!…]/.test(char)
+                ? this.randomizeTime(3 * time)
+                : this.randomizeTime(time);
+
+            if (
+                primaryColorStart != null &&
+                primaryColorEnd != null &&
+                this.counter >= primaryColorStart &&
+                this.counter <= primaryColorEnd
+            ) {
+                let newHTML =
+                    this.text.slice(0, primaryColorStart) +
+                    '<span class="primary-color-text">' +
+                    this.text.slice(primaryColorStart, this.counter) +
+                    this.text.charAt(this.counter);
+
+                if (this.counter === primaryColorEnd) newHTML += "</span>";
+                this.element.innerHTML = newHTML;
+            } else this.element.innerHTML += this.text.charAt(this.counter);
+
+            this.counter++;
+
+            setTimeout(
+                () =>
+                    this.write(
+                        this.element,
+                        time,
+                        primaryColorStart,
+                        primaryColorEnd,
+                        useElementText,
+                        text
+                    ),
+                delay
+            );
+        } else {
+            let char = this.text.charAt(this.counter);
+
+            this.element.classList.remove("writing");
+            this.element = null;
+            this.counter = 0;
+
+            if (this.queue.length) {
+                const nextItem = this.queue.shift();
+                let delay = /[.,،؟!…]/.test(char)
+                    ? this.randomizeTime(6 * nextItem.time)
+                    : this.randomizeTime(5 * nextItem.time);
+
+                setTimeout(
+                    () =>
+                        this.write(
+                            nextItem.element,
+                            nextItem.time,
+                            nextItem.primaryColorStart,
+                            nextItem.primaryColorEnd,
+                            nextItem.useElementText,
+                            nextItem.text
+                        ),
+                    delay
+                );
+            } else return;
+        }
     }
 
-    if (this.counter === 0) {
-      this.element = element;
-      this.element.classList.add("writing");
-      this.text = useElementText ? element.innerText : text;
-      this.element.innerHTML = "";
+    randomizeTime(time = 100) {
+        return time + Math.floor(Math.random() * time * 0.2);
     }
-
-    if (this.counter < this.text.length) {
-      if (
-        primaryColorStart != null &&
-        primaryColorEnd != null &&
-        this.counter >= primaryColorStart &&
-        this.counter <= primaryColorEnd
-      ) {
-        let newHTML =
-          this.text.slice(0, primaryColorStart) +
-          '<span class="primary-color-text">' +
-          this.text.slice(primaryColorStart, this.counter) +
-          this.text.charAt(this.counter);
-
-        if (this.counter === primaryColorEnd) newHTML += "</span>";
-        this.element.innerHTML = newHTML;
-      } else this.element.innerHTML += this.text.charAt(this.counter);
-      this.counter++;
-      setTimeout(
-        () =>
-          this.write(
-            this.element,
-            time,
-            primaryColorStart,
-            primaryColorEnd,
-            useElementText,
-            text
-          ),
-        time
-      );
-    } else {
-      this.element.classList.remove("writing");
-      this.element = null;
-      this.counter = 0;
-
-      if (this.queue.length) {
-        const nextItem = this.queue.shift();
-        setTimeout(
-          () =>
-            this.write(
-              nextItem.element,
-              nextItem.time,
-              nextItem.primaryColorStart,
-              nextItem.primaryColorEnd,
-              nextItem.useElementText,
-              nextItem.text
-            ),
-          nextItem.time
-        );
-      } else return;
-    }
-  }
 }
